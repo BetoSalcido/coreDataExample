99799cd95015610f5fe4bdfc2132a5bea077d045
BetoSalcido <betho.salcidoo@gmail.com>
2020-05-25 13:15:50 -0600

    Initial commit

---
+++ codeData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  codeData
+//
+//  Created by Beto Salcido on 25/05/20.
+//  Copyright © 2020 BetoSalcido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
